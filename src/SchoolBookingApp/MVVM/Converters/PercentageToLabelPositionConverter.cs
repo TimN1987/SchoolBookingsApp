a76239ca0a6cbfb202096619bdb2bea685d4d292
TimN1987 <timothy.naish@gmail.com>
2025-09-08 21:56:06 +0100

    Commenting added for the PercentageToLabelPositionConverter class.

--- src/SchoolBookingApp/MVVM/Converters/PercentageToLabelPositionConverter.cs
+++ src/SchoolBookingApp/MVVM/Converters/PercentageToLabelPositionConverter.cs
@@ -11,6 +11,18 @@ namespace SchoolBookingApp.MVVM.Converters
 {
     public class PercentageToLabelPositionConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a percentage value to a position for a pie chart label. Uses the parameter to determine which segment 
+        /// of the pie chart the label is for and whether to calculate the X or Y position. Used to position labels for 
+        /// the booked and unbooked segments of the pie chart.
+        /// </summary>
+        /// <param name="value">The percentage represented by the segment to be labelled.</param>
+        /// <param name="parameter">The converter parameter given in the view. Indicates which segment is being 
+        /// labelled and whether the X or Y position is required.</param>
+        /// <returns>A <see langword="double"/> value giving the X or Y value to set the <c>Top</c> or <c>Left</c> 
+        /// position for the label.</returns>
+        /// <remarks>The converter parameter should consist of two characters: X or Y to indicate which axis is being set, 
+        /// and B or U to indicate whether the booked or unbooked segment is being labelled.</remarks>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is double percentage && parameter is string labelInfo)
