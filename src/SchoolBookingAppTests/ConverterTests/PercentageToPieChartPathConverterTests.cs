d1c463eea3d6122548db2074a38b54bf497d3145
TimN1987 <timothy.naish@gmail.com>
2025-09-08 22:19:06 +0100

    ConvertBack method test added for the PercentageToPieChartPathConverter class.

--- src/SchoolBookingAppTests/ConverterTests/PercentageToPieChartPathConverterTests.cs
+++ src/SchoolBookingAppTests/ConverterTests/PercentageToPieChartPathConverterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,45 @@ namespace SchoolBookingAppTests.ConverterTests
             //Assert
             Assert.NotNull(converter);
             Assert.IsType<PercentageToPieChartPathConverter>(converter);
+        }
+
+        //Convert tests.
+
+        //ConvertBack test.
+        /// <summary>
+        /// Verifies that a <see cref="NotSupportedException"/> is thrown when the <see cref="PercentageToPieChartPathConverter
+        /// .ConvertBack"/> method is called. Uses different types and values for the parameters to test that this 
+        /// exception is always thrown avoiding any unexpected exceptions.
+        /// </summary>
+        /// <param name="value">The value passed from the view to the viewmodel.</param>
+        /// <param name="targetType">The expected type of value passed from the view to the viewmodel, if given.</param>
+        /// <param name="parameter">The converter parameter given in the view.</param>
+        /// <param name="culture">The culture information used in any conversion.</param>
+        [Theory]
+        [MemberData(nameof(ConvertBackTestMemberData))]
+        public void ConvertBack_AlwaysThrowsNotSupportedException(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Arrange, Act & Assert
+            Assert.Throws<NotSupportedException>(() => _converter.ConvertBack(value, targetType, parameter, culture));
+        }
+
+        //MemberData
+        /// <summary>
+        /// Provides member data for the convert back test. Uses different values for each parameter to ensure a variety 
+        /// of cases are tested where each case should thrown a <see cref="NotSupportedException"/>.
+        /// </summary>
+        public static IEnumerable<object?[]> ConvertBackTestMemberData()
+        {
+            yield return new object?[] { null, null, null, null };
+            yield return new object?[] { 50.0, typeof(double), "YB", CultureInfo.InvariantCulture };
+            yield return new object?[] { "convert", typeof(string), 17, CultureInfo.CurrentCulture };
+            yield return new object?[] { 100, typeof(int), 'c', CultureInfo.InvariantCulture };
+            yield return new object?[] { 'b', typeof(char), 36.0, CultureInfo.CurrentUICulture };
+            yield return new object?[] { 150.0, typeof(DateTime), 3, CultureInfo.InvariantCulture };
+            yield return new object?[] { "convert", null, DateTime.Now, CultureInfo.CurrentUICulture };
+            yield return new object?[] { 50.0, typeof(string), null, CultureInfo.CurrentUICulture };
+            yield return new object?[] { DateTime.Now, typeof(long), 'e', null };
         }
     }
 }
