477e4d8c22bb7f61e24db84cc4cd81eb12225a16
TimN1987 <timothy.naish@gmail.com>
2025-09-09 16:40:15 +0100

    Added IReadOperationService parameter to the constuctor for the AddBookingViewModel to ensure that student information and data can be fully displayed. Added relevant tests.

--- src/SchoolBookingApp/MVVM/Viewmodel/AddBookingViewModel.cs
+++ src/SchoolBookingApp/MVVM/Viewmodel/AddBookingViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SchoolBookingApp.MVVM.Database;
+using SchoolBookingApp.MVVM.Model;
 using SchoolBookingApp.MVVM.Services;
 using SchoolBookingApp.MVVM.Viewmodel.Base;
 
@@ -14,7 +15,9 @@ namespace SchoolBookingApp.MVVM.Viewmodel
         //Fields
         private readonly IEventAggregator _eventAggregator;
         private readonly IBookingManager _bookingManager;
+        private readonly IReadOperationService _readOperationService;
         private Booking _booking;
+        private Student? _bookedStudent;
         private List<Booking> _allBookings;
         private bool _isNewBooking;
 
@@ -24,20 +27,29 @@ namespace SchoolBookingApp.MVVM.Viewmodel
             get => _booking;
             set => SetProperty(ref _booking, value);
         }
+        public Student? BookedStudent
+        {
+            get => _bookedStudent;
+            set => SetProperty(ref _bookedStudent, value);
+        }
         public List<Booking> AllBookings
         {
             get => _allBookings;
             set => SetProperty(ref _allBookings, value);
         }
 
-        public AddBookingViewModel(IEventAggregator eventAggregator, IBookingManager bookingManager)
+        public AddBookingViewModel(
+            IEventAggregator eventAggregator, IBookingManager bookingManager, IReadOperationService readOperationService)
         {
             _eventAggregator = eventAggregator 
                 ?? throw new ArgumentNullException(nameof(eventAggregator));
             _bookingManager = bookingManager
                 ?? throw new ArgumentNullException(nameof(bookingManager));
+            _readOperationService = readOperationService 
+                ?? throw new ArgumentNullException(nameof(readOperationService));
 
             _booking = new Booking(0, string.Empty, string.Empty, string.Empty, string.Empty);
+            _bookedStudent = null;
             _allBookings = _bookingManager.ListBookings().GetAwaiter().GetResult();
             _isNewBooking = true;
 
@@ -46,6 +58,7 @@ namespace SchoolBookingApp.MVVM.Viewmodel
                 if (param is Booking booking)
                 {
                     Booking = booking;
+                    BookedStudent = _readOperationService.GetStudentData(Booking.StudentId).GetAwaiter().GetResult();
                     _isNewBooking = false;
                 }
             });
