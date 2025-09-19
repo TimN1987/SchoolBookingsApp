4289c01be2138b08efb36e8ab97f16225c2d1fcd
TimN1987 <timothy.naish@gmail.com>
2025-09-19 10:26:05 +0100

    Removed parent selection from the AddStudentViewModel. The database methods are set for adding students to a parent instead. Ensured testing complete for DisplayStudentDetails.

--- src/SchoolBookingAppTests/ViewModelTests/AddStudentViewModelTests.cs
+++ src/SchoolBookingAppTests/ViewModelTests/AddStudentViewModelTests.cs
@@ -18,13 +18,14 @@ namespace SchoolBookingAppTests.ViewModelTests
         private readonly Mock<IDeleteOperationService> _deleteOperationServiceMock;
 
         private readonly AddStudentViewModel _viewModel;
+        private readonly AddStudentViewModel _viewModelWithSelectedStudent;
+
         private readonly Student _testStudent;
         private readonly Parent _testParent;
         private readonly SearchResult _testSearchResult;
         private readonly string _testString;
         private readonly int _testDateOfBirthInt;
-        private readonly string _testRelationship;
-
+        private readonly DateTime _testDateOfBirth;
         public AddStudentViewModelTests()
         {
             _readOperationServiceMock = new Mock<IReadOperationService>();
@@ -32,12 +33,25 @@ namespace SchoolBookingAppTests.ViewModelTests
             _updateOperationServiceMock = new Mock<IUpdateOperationService>();
             _deleteOperationServiceMock = new Mock<IDeleteOperationService>();
 
+            _testString = "test string";
+            _testDateOfBirthInt = 20250911;
+            _testDateOfBirth = new DateTime(2025, 9, 11);
+            _testStudent = new Student(1, _testString, _testString, _testDateOfBirthInt, _testString, [], new(), new());
+            _testParent = new Parent(1, _testString, _testString, []);
+            _testSearchResult = new SearchResult()
+            {
+                Id = 1,
+                FirstName = _testString,
+                LastName = _testString,
+                Category = _testString
+            };
+
             _readOperationServiceMock
                 .Setup(x => x.GetStudentList())
                 .Returns(Task.FromResult<List<SearchResult>>([]));
             _readOperationServiceMock
-                .Setup(x => x.GetParentList())
-                .Returns(Task.FromResult<List<SearchResult>>([]));
+                .Setup(x => x.GetStudentData(It.IsAny<int>()))
+                .Returns(Task.FromResult<Student>(_testStudent));
 
             _viewModel = new AddStudentViewModel(
                 _readOperationServiceMock.Object,
@@ -45,13 +59,6 @@ namespace SchoolBookingAppTests.ViewModelTests
                 _updateOperationServiceMock.Object,
                 _deleteOperationServiceMock.Object
                 );
-
-            _testString = "test string";
-            _testDateOfBirthInt = 20250911;
-            _testStudent = new Student(1, _testString, _testString, _testDateOfBirthInt, _testString, [], new(), new());
-            _testParent = new Parent(1, _testString, _testString, []);
-            _testSearchResult = new SearchResult();
-            _testRelationship = "Relationship";
         }
 
         //Constructor tests.
@@ -135,43 +142,64 @@ namespace SchoolBookingAppTests.ViewModelTests
             Assert.IsType<AddStudentViewModel>(viewModel);
         }
 
-        //Property tests.
+        //DisplayStudentDetails tests.
 
         /// <summary>
-        /// Verifies that the <see cref="AddStudentViewModel.SelectedNewParent"/> property is set to <see langword="null"/> 
-        /// when the <see cref="AddStudentViewModel.SelectedCurrentParent"/> property is updated. Ensures that only one 
-        /// parent can be selected.
+        /// Verifies that when <see cref="AddStudentViewModel.DisplayStudentDetails"/> is called with a valid <see 
+        /// cref="SearchResult"/> set to <see cref="AddStudentViewModel.SelectedStudent"/>, the UI properties are updated 
+        /// to reflect the details of the selected student. Ensures that the displayed student information is correct.
         /// </summary>
         [Fact]
-        public void SelectedCurrentParent_ValueUpdated_SelectedNewParentSetToNull()
-        {
-            //Arrange & Act
-            _viewModel.SelectedCurrentParent = (_testParent, _testRelationship);
-
-            //Assert
-            Assert.Null(_viewModel.SelectedNewParent);
-        }
-
-        /// <summary>
-        /// Verifies that the UI parent detail properties are updated as expected when the <see cref="AddStudentViewModel.
-        /// SelectedCurrentParent"/> property is updated. Ensures that the UI displays the correct values to avoid confusion.
-        /// </summary>
-        [Fact]
-        public void SelectedCurrentParent_ValueUpdated_ParentDetailsUpdatedToExpectedValues()
+        public async Task DisplayStudentDetails_ValidSelectedStudentId_UIPropertiesUpdatedCorrectly()
         {
             //Arrange
-            string expectedParentName = _testStudent.FirstName + " " + _testStudent.LastName;
+            _viewModel.SelectedStudent = _testSearchResult;
 
             //Act
-            _viewModel.SelectedCurrentParent = (_testParent, _testRelationship);
+            await _viewModel.DisplayStudentDetails();
 
             //Assert
-            Assert.NotNull(_viewModel.SelectedCurrentParent);
-            Assert.Equal(_testRelationship, _viewModel.Relationship);
-            Assert.Equal(expectedParentName, _viewModel.ParentName);
+            Assert.Equal(_testString, _viewModel.FirstName);
+            Assert.Equal(_testString, _viewModel.LastName);
+            Assert.Equal(_testDateOfBirth, _viewModel.DateOfBirth);
+            Assert.Equal(_testString, _viewModel.ClassName);
         }
 
+        /// <summary>
+        /// Verifies that when <see cref="AddStudentViewModel.DisplayStudentDetails"/> is called with an invalid <see 
+        /// cref="SearchResult"/> (null or Id <= 0) set for the <see cref="AddStudentViewModel.SelectedStudent"/> property, 
+        /// the UI properties remain unchanged. Ensures that no erroneous data is displayed when an invalid
+        /// </summary>
+        /// <remarks>Uses a new viewmodel instance to ensure the key proeprties are set to their default values to ensure 
+        /// testing consistency.</remarks>
+        [Fact]
+        public async Task DisplayStudentDetails_InvalidSelectedStudent_UIPropertiesRemainUnchanged()
+        {
+            //Arrange
+            var invalidSearchResult = new SearchResult()
+            {
+                Id = -1,
+                FirstName = "Invalid",
+                LastName = "Student",
+                Category = "Student"
+            };
+            var viewModel = new AddStudentViewModel(
+                _readOperationServiceMock.Object,
+                _createOperationServiceMock.Object,
+                _updateOperationServiceMock.Object,
+                _deleteOperationServiceMock.Object);
+            viewModel.SelectedStudent = invalidSearchResult;
 
-        //Member Data
+            //Act
+            await viewModel.DisplayStudentDetails();
+
+            //Assert
+            Assert.Equal(string.Empty, viewModel.FirstName);
+            Assert.Equal(string.Empty, viewModel.LastName);
+            Assert.Equal(DateTime.MinValue, viewModel.DateOfBirth);
+            Assert.Equal(string.Empty, viewModel.ClassName);
+        }
+
+        //AddUpdateStudent tests.
     }
 }
