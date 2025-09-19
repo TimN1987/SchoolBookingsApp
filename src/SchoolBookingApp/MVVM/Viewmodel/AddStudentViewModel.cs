aa24cbce07ff779a0ad02a38d839f7491c6627b5
TimN1987 <timothy.naish@gmail.com>
2025-09-19 12:40:45 +0100

    Created AddStudentView and set width/height for the Grid container.

--- src/SchoolBookingApp/MVVM/Viewmodel/AddStudentViewModel.cs
+++ src/SchoolBookingApp/MVVM/Viewmodel/AddStudentViewModel.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using SchoolBookingApp.MVVM.Commands;
 using SchoolBookingApp.MVVM.Database;
