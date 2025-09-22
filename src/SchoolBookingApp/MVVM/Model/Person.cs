a68acd865adea2819e92d15533ea46c82a392e90
TimN1987 <timothy.naish@gmail.com>
2025-09-22 10:47:49 +0100

    Added converter to ensure data bindings work for Children list.

---
+++ src/SchoolBookingApp/MVVM/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBookingApp.MVVM.Model
+{
+    /// <summary>
+    /// An abstract class for creating people for the database.
+    /// </summary>
+    /// <param name="Id">The unique id number from the database.</param>
+    /// <param name="FirstName">The person's first name.</param>
+    /// <param name="LastName">The person's last name.</param>
+    public abstract record Person(int Id, string FirstName, string LastName);
+}
---
+++ src/SchoolBookingApp/MVVM/Model/Relationship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBookingApp.MVVM.Model
+{
+    /// <summary>
+    /// A class for storing a parent-student relationships, containing the selected person and the parent relationship to that 
+    /// student. Used for lists of children, lists of parents or enabling data bindings to display children/parents.
+    /// </summary>
+    public record Relationship(Person Person, string RelationshipType);
+}
