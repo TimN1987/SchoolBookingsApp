27044ffd2e31f46a59b306538802fe2af3d9ca6d
TimN1987 <timothy.naish@gmail.com>
2025-10-08 11:00:18 +0100

    Organised private fields.

--- src/SchoolBookingAppTests/ViewModelTests/SearchViewModelTests.cs
+++ src/SchoolBookingAppTests/ViewModelTests/SearchViewModelTests.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using SchoolBookingApp.MVVM.Database;
 
 namespace SchoolBookingAppTests.ViewModelTests
 {
     public class SearchViewModelTests
     {
+        private readonly Mock<IEventAggregator> _eventAggregatorMock;
+        private readonly Mock<IReadOperationService> _readOperationServiceMock;
+
+        public SearchViewModelTests()
+        {
+            _eventAggregatorMock = new Mock<IEventAggregator>();
+            _readOperationServiceMock = new Mock<IReadOperationService>();
+        }
     }
 }
